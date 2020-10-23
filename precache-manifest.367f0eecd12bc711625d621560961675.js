f3efa37e95407543099d7de891cc94d650a83817
Tomasz Jarkowski <tomaszjarkowski1998@gmail.com>
2020-10-23 13:16:16 +0200

    Updates

---
+++ precache-manifest.367f0eecd12bc711625d621560961675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4a335f932559fe0e5cae002ff3a3d5",
+    "url": "/reduxproject/index.html"
+  },
+  {
+    "revision": "0e0605baaf277616b48d",
+    "url": "/reduxproject/static/css/main.2ac0bfeb.chunk.css"
+  },
+  {
+    "revision": "df3b903e06b4b1d665e2",
+    "url": "/reduxproject/static/js/2.eb094a50.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/reduxproject/static/js/2.eb094a50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e0605baaf277616b48d",
+    "url": "/reduxproject/static/js/main.8c8876c9.chunk.js"
+  },
+  {
+    "revision": "fec8f2b7b3ec399d802f",
+    "url": "/reduxproject/static/js/runtime-main.d793c559.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/redux/precache-manifest.377dd012b7fe05219ae1e920b1e98313.js"
+  "/reduxproject/precache-manifest.367f0eecd12bc711625d621560961675.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redux/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reduxproject/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
